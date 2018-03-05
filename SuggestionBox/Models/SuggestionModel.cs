b9ad3b5cc11dffe82a7fb696ec29f2043cb88bf3
ABardot <aabardot5@gmail.com>
2018-03-05 14:25:35 -0500

    Initial commit

---
+++ SuggestionBox/Models/SuggestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuggestionBox.Models
+{
+    public class SuggestionModel
+    {   //Always have an ID in Model
+        [Key] //Key data annotation which creates a unique identifier  
+        public int SuggestionId { get; set; }
+
+        public string Topic { get; set; }
+        public string Suggestion { get; set; } // Anytime you add a model you should always build Ctrl + Shift + b
+
+
+    }
+}
